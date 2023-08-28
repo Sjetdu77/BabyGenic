9f777694be5c91a75ff582250b73a0b38c2ba32b
AdameBf <adame.bf@gmail.com>
2023-08-28 16:50:40 +0200

    Fix separator

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-raw_data = pd.read_csv('German_cities_raw_data.csv', encoding='utf-16le')
+raw_data = pd.read_csv('German_cities_raw_data.csv', encoding='utf-16le', sep='\t')
 print(raw_data.head())
