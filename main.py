9161225eb04ff3a3c51f0ef0cca83409c4648fba
Andy Frot <andy.frot@outlook.fr>
2023-08-29 15:52:39 +0200

    Group of first länder

--- main.py
+++ main.py
@@ -13,11 +13,20 @@ print(per_population_data[relevant_cols][:15])
 # Group by Land. Determine the most populated Länder
 data_grouped = raw_data.groupby("federal state")
 
-all_results = {"federal state": [], "population 2015": []}
+all_results = {"federal state": [], "population 2015": [], "area": [], "density": []}
 
 for name, group in data_grouped:
     all_results["federal state"].append(name)
-    all_results["population 2015"].append(group["population 2015"].sum())
+    population = group["population 2015"].sum()
+    area = group["area [km²]-2015"].sum()
+    all_results["population 2015"].append(population)
+    all_results["area"].append(area)
+    all_results["density"].append(population // area)
 
-table_federal_state = pd.DataFrame(all_results).sort_values(by="population 2015", ascending=False)
-print(table_federal_state.head())
+table_federal_state = pd.DataFrame(all_results).sort_values(by="density", ascending=False)
+
+first_land = data_grouped.get_group(table_federal_state.iloc[0]["federal state"])
+second_land = data_grouped.get_group(table_federal_state.iloc[1]["federal state"])
+
+print(first_land.sort_values(by="population 2015"))
+print(second_land.sort_values(by="population 2015"))
