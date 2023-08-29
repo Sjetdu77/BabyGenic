ae98ff0ac75d8a7d6a7abc6e5e582f625c502187
AdameBf <adame.bf@gmail.com>
2023-08-29 10:10:50 +0200

    Filter out irrelevant columns, display top 15 cities per population in descending order

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import pandas as pd
 
+# Data loading
 raw_data = pd.read_csv('German_cities_raw_data.csv', encoding='utf-16le', sep='\t')
 print(raw_data.head())
+
+# Filter: Relevant columns
+relevant_cols = ['city', 'federal state', 'population 2015', 'delta [%] (2014-2015)']
+
+# Order by: Population (desc)
+per_population_data = raw_data.sort_values(by='population 2015', ascending=False)
+print(per_population_data[relevant_cols][:15])
